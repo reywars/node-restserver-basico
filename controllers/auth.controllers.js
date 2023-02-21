4814f5e4b1b547f9a7a81771146c4e2210922afd
Rey <yarimagb@gmail.com>
2023-02-21 22:53:32 +0100

    Fin sección 11 - version 4.0.0

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,8 +1,10 @@
-const { response, request } = require('express');
+const { response, request, json } = require('express');
 const bcryptjs  = require('bcryptjs');
 
 const User = require('../models/user.models');
+
 const { generateJWT } = require('../helpers/generate-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req = request, res = response) => {
@@ -50,6 +52,56 @@ const login = async (req = request, res = response) => {
     }
 }
 
+const googleSingIn = async (req = request, res = response) => {
+
+    const { id_token } = req.body;
+
+    try {
+        const { name, email, img } = await googleVerify( id_token );
+
+        let user = await User.findOne({ email });
+
+        if( !user ){
+            // Lo creamos
+            const data = {
+                name,
+                email,
+                password: ':P',
+                img,
+                google: true,
+                role: 'USER_ROLE'
+
+            };
+            user = new User( data );
+            await user.save();
+        }
+
+        // Si el usuario en BD está activo
+        if ( !user.status ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generateJWT(user.id);
+
+        res.json({
+            user,
+            token
+        }); 
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+}
+
+
 module.exports = {
-    login
+    login,
+    googleSingIn
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validateFields } = require('../middlewares/validate-fields');
-const { login } = require('../controllers/auth.controllers');
+const { login, googleSingIn } = require('../controllers/auth.controllers');
 
 
 const router = Router();
@@ -13,5 +13,10 @@ router.post('/login',[
     check('password', 'El password es obligatorio').not().isEmpty(),
     validateFields
 ], login);
+
+router.post('/google',[
+    check('id_token', 'El id_token de google es necesario').not().isEmpty(),
+    validateFields
+], googleSingIn);
 
 module.exports = router;
